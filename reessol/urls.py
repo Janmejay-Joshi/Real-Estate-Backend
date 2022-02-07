3c8004881be72d7de87e86b055a53d1dc32cacfe
Janmejay-Joshi <publish.janmejay@gmail.com>
2022-02-08 01:33:10 +0530

    Migrations

--- reessol/urls.py
+++ reessol/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path("api/", include("apps.profiles.urls")),
     path("api/", include("apps.properties.urls")),
     path("auth/", include("dj_rest_auth.urls")),
-    path("auth/registration/", include("dj_rest_auth.registration.urls")),
+    path("auth/register/", include("dj_rest_auth.registration.urls")),
 ]
