36c857d2a279b665eb394b570a00a893d624909d
Janmejay-Joshi <publish.janmejay@gmail.com>
2022-02-11 18:45:42 +0530

    Expanded User Seriallizer

--- apps/profiles/serializers.py
+++ apps/profiles/serializers.py
@@ -4,6 +4,14 @@ from apps.profiles.models import UserProfileModel
 from apps.properties.models import PropertyModel
 from rest_flex_fields import FlexFieldsModelSerializer
 
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
 
 class UserProfileSerializer(FlexFieldsModelSerializer):
     """
@@ -17,11 +25,13 @@ class UserProfileSerializer(FlexFieldsModelSerializer):
         fields = "__all__"
         read_only_fields = (
             "is_prime",
+            "is_verified",
             "user",
         )
 
         expandable_fields = {
             "image": ("apps.properties.ImageSerializer"),
+            "user": ("apps.profiles.UserSerializer"),
             "properties": ("apps.properties.PropertySerializer", {"many": True}),
             "wishlist": ("apps.properties.PropertySerializer", {"many": True}),
         }
