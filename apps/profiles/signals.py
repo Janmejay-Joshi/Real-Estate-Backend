61a851f092a4e78e70c36e39e4ba659a474b1b14
Janmejay-Joshi <publish.janmejay@gmail.com>
2022-02-20 02:52:34 +0530

    Phone base

--- apps/profiles/signals.py
+++ apps/profiles/signals.py
@@ -1,3 +1,4 @@
+from random import randint
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
@@ -11,7 +12,6 @@ def create_user_profile(sender, instance, created, **kwargs):
     if created:
         prime = PrimeModel.objects.create()
         user = UserProfileModel.objects.create(
-            user=instance,
-            prime_status=prime,
+            user=instance, prime_status=prime, phone=f"{randint(0,10000000)}"
         )
         OTPModel.objects.create(user=user)
